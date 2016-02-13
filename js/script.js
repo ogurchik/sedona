63bb4dbf0e8e3e44a077b3a20490ad3e149d7b07
lpkn <silvervaza@gmail.com>
2016-02-13 20:04:41 +0200

    slidemenu

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(".clica").mouseenter(function(){
+        $(".clica").fadeTo('slow', 0.25);
+        $(document).alert('fast');
+    });
+var $p = $('p');
+alert($p);
+$('.btn-search-to-hotel').click(function(){
+    $(".slide-menu").toggle();
+});
